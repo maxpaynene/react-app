86495ff0e5063ed8b2179e44df6484d7f341c337
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-26 02:55:23 -0300

    STATES

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,81 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+class App extends React.Component {
+  render(){
+    return <div>
+      this is my component:
+      <Helloworld hello="Hello" tittle="tittle"/>
+      <Helloworld hello="world" tittle="two"/>
+      <Helloworld hello="world"/>
+    </div>
+  }
+}
+
+/*
+//ESTE ES EL MISMO CODIGO DE ARRIBA PERO ESCRITO DE OTRA FORMA
+const App = () => <div>this is my component <Helloworld/> <Helloworld/> <Helloworld/></div>
+*/
+
+/*
+//ESTE ES EL MISMO CODIGO DE ARRIBA PERO ESCRITO DE OTRA FORMA
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      this is my component: 
+      <Helloworld/>
+      <Helloworld/>
+      <Helloworld/>
     </div>
   );
 }
+*/
+
+class Helloworld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  togleShow = () => {
+    this.setState({ show: !this.state.show });
+  }
+
+  render(){
+    if(this.state.show){
+      return (
+        <div id="helloworld">
+          <h3>
+            {this.props.tittle}
+          </h3>      
+          {this.props.hello}
+          <button onClick={()=> this.togleShow()}>Togle show</button>
+        </div>
+      );
+    }else{
+      return (
+        <div>
+          <h1>No hay Elementos.</h1>
+          <button onClick={()=> this.togleShow()}>Togle show</button>
+        </div>
+      );
+    }
+  }
+}
+
+/*
+//ESTE ES EL MISMO CODIGO QUE ESTA ESCRITO ARRIBA PERO DE OTRA FORMA.
+function Helloworld(props) {
+  console.log(props);
+  return (
+    <div id="helloworld">
+      <h3>
+        {props.tittle}
+      </h3>      
+      {props.hello}
+    </div>
+  );
+}
+*/
 
 export default App;
