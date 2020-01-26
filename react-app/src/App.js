e1517f2f924dd7582d62550a860268a3d3c46e45
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-26 14:41:34 -0300

    react component with class

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,81 +1,17 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './App.css';
+import tasks from './sample/task.json';
+import Tasks from './components/Tasks';
 
-class App extends React.Component {
-  render(){
-    return <div>
-      this is my component:
-      <Helloworld hello="Hello" tittle="tittle"/>
-      <Helloworld hello="world" tittle="two"/>
-      <Helloworld hello="world"/>
-    </div>
-  }
-}
-
-/*
-//ESTE ES EL MISMO CODIGO DE ARRIBA PERO ESCRITO DE OTRA FORMA
-const App = () => <div>this is my component <Helloworld/> <Helloworld/> <Helloworld/></div>
-*/
-
-/*
-//ESTE ES EL MISMO CODIGO DE ARRIBA PERO ESCRITO DE OTRA FORMA
-function App() {
-  return (
-    <div>
-      this is my component: 
-      <Helloworld/>
-      <Helloworld/>
-      <Helloworld/>
-    </div>
-  );
-}
-*/
-
-class Helloworld extends React.Component {
+class App extends Component {
 
   state = {
-    show: true
-  }
-
-  togleShow = () => {
-    this.setState({ show: !this.state.show });
+    tasks: tasks
   }
 
   render(){
-    if(this.state.show){
-      return (
-        <div id="helloworld">
-          <h3>
-            {this.props.tittle}
-          </h3>      
-          {this.props.hello}
-          <button onClick={()=> this.togleShow()}>Togle show</button>
-        </div>
-      );
-    }else{
-      return (
-        <div>
-          <h1>No hay Elementos.</h1>
-          <button onClick={()=> this.togleShow()}>Togle show</button>
-        </div>
-      );
-    }
+    return <Tasks tasks={this.state.tasks}/>
   }
 }
-
-/*
-//ESTE ES EL MISMO CODIGO QUE ESTA ESCRITO ARRIBA PERO DE OTRA FORMA.
-function Helloworld(props) {
-  console.log(props);
-  return (
-    <div id="helloworld">
-      <h3>
-        {props.tittle}
-      </h3>      
-      {props.hello}
-    </div>
-  );
-}
-*/
 
 export default App;
