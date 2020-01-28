683f2220246a023e72c0ea3248c68ed78fcdb943
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-27 22:48:09 -0300

    form finish

--- react-app/src/components/TaskForm.js
+++ react-app/src/components/TaskForm.js
@@ -2,22 +2,39 @@ import React, {Component} from 'react';
 
 export default class TaskForm extends Component {
 
+    state = {
+        tittle: '',
+        description: ''
+    }
+
     onSubmit = e => {
-        console.log('submit');
+        console.log('submit',this.state);
         e.preventDefault();
     }
 
     onChange = e => {
-        console.log(e.target.value);
+        console.log([e.target.name],[e.target.value]);
+        this.setState({[e.target.name]:[e.target.value]});
     }
 
     render(){
         return (
             <form onSubmit={this.onSubmit}>
-                <input type="text" placeholder="write a task" onChange={this.onChange} />
+                <input 
+                    type="text" 
+                    placeholder="write a task" 
+                    onChange={this.onChange} 
+                    value={this.state.tittle} 
+                    name="tittle"
+                />
                 <br/>
                 <br/>
-                <textarea placeholder="Write a Description" onChange={this.onChange} />
+                <textarea 
+                    placeholder="Write a Description" 
+                    onChange={this.onChange} 
+                    value={this.state.description}
+                    name="description"
+                />
                 <br/>
                 <br/>
                 <input type="submit" />
